e200f70bf6834cc97a8ec9c2a3581679dbf8790e
4gotspoo <onehalfgiraffe@gmail.com>
2019-08-22 16:02:56 -0400

    Added messages controller and allowed posting of new messages

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,9 @@
 class ChatroomsController < ApplicationController
+  before_action :require_user
 
 	def index
     @messages = Message.all
     @users = User.all
+    @message = Message.new
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
 	def new
 		
@@ -22,5 +23,13 @@ class SessionsController < ApplicationController
 		flash[:success] = "You have logged out."
 		redirect_to login_path
 	end
-
+  
+  private
+  
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "Cannot login: you are already logged in."
+      redirect_to root_path
+    end
+  end
 end
